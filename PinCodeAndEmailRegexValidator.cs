528e373c1e7c03778b12ad0ba9b2559ea757434c
praveenupadhyay997 <praveenupadhyay997@gmail.com>
2020-10-06 16:16:25 +0530

    [Praveen] Add: Added to validate the second use case with not more than six characters and not begining with alphabet or special character

--- PinCodeAndEmailRegexValidator.cs
+++ PinCodeAndEmailRegexValidator.cs
@@ -7,13 +7,13 @@ namespace PinCodeAndEmailEvaluator
 {
     class PinCodeAndEmailRegexValidator
     {
-        public static string PIN_REGEXUC1 = "^[0-9]{6}$";
+        public static string PIN_REGEXUC2 = "^(?![a-zA-Z#$%&'*+/=?^_`{|}~-][0-9]).{6}$";
         public static string pinCode;
         public static string emailID;
 
         public bool maximumDigitValidator(string pinCode)
         {
-            return Regex.IsMatch(pinCode, PIN_REGEXUC1);
+            return Regex.IsMatch(pinCode, PIN_REGEXUC2);
         }
     }
 }
