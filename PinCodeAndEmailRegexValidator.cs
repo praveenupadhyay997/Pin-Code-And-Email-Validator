32b6dfe47bdfe8f8fefe21b0f1a2b8133caf9dfe
praveenupadhyay997 <praveenupadhyay997@gmail.com>
2020-10-06 15:06:32 +0530

    [Praveen] Add: Welcome Message for the pin code and Email Validator

---
+++ PinCodeAndEmailRegexValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PinCodeAndEmailEvaluator
+{
+    class PinCodeAndEmailRegexValidator
+    {
+        public static string PIN_REGEXUC1 = "^[0-9]{6}$";
+        public static string pinCode;
+        public static string emailID;
+
+        public bool maximumDigitValidator(string pinCode)
+        {
+            return Regex.IsMatch(pinCode, PIN_REGEXUC1);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace PinCodeAndEmailEvaluator
+{
+    class Program
+    {
+        public static string pinCode;
+        public static string emailId;
+        public static PinCodeAndEmailRegexValidator pinCodeAndEmailRegex = new PinCodeAndEmailRegexValidator();
+
+        static void Main(string[] args)
+        {   
+           
+            Console.WriteLine("===========================================");
+            Console.WriteLine("Welcome to the Pin Code and Email Validator");
+            Console.WriteLine("===========================================");           
+        }
+    }
+}
